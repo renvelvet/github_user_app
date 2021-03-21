2cccec650d7ab3b84b79afc89d2892f870b218b9
renvelvet <reshapuspita734@gmail.com>
2021-03-22 02:34:56 +0700

    add model class + assets

---
+++ app/src/main/java/com/dicoding/githubuserapp/User.kt
@@ -0,0 +1,16 @@
+package com.dicoding.githubuserapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User(
+    val username: String?,
+    val name: String?,
+    val avatar: String?,
+    val company: String?,
+    val location: String?,
+    val repository: Int?,
+    val follower: Int?,
+    val following: Int?
+) : Parcelable
