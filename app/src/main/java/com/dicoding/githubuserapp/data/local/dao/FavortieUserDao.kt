60748fe2029a0a30a9aaadb5c365a15a4f98a08c
renvelvet <reshapuspita734@gmail.com>
2021-04-23 00:12:19 +0700

    fix FavoriteUserDao

--- app/src/main/java/com/dicoding/githubuserapp/data/local/dao/FavortieUserDao.kt
+++ app/src/main/java/com/dicoding/githubuserapp/data/local/dao/FavortieUserDao.kt
@@ -10,10 +10,10 @@ import com.dicoding.githubuserapp.data.local.entity.FavoriteUser
 @Dao
 interface FavortieUserDao {
     @Query("SELECT * FROM favorite_user")
-    fun getAll(): LiveData<ArrayList<FavoriteUser>>
+    fun getAll(): LiveData<List<FavoriteUser>>
 
     @Query("SELECT * FROM favorite_user WHERE username LIKE :favUsername LIMIT 1")
-    fun findByUsername(favUsername: String) : LiveData<ArrayList<FavoriteUser>>
+    fun findByUsername(favUsername: String) : LiveData<List<FavoriteUser>>
 
     @Insert
     fun addToFavorite(user: FavoriteUser)
